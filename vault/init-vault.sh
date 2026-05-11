c27a24cebb1f45c7afef84fa3d95775bcb1c9d6a
DemonStage <DemonStage0@users.noreply.github.com>
2026-05-11 15:23:32 +0300

    fixes

--- vault/init-vault.sh
+++ vault/init-vault.sh
@@ -1,22 +1,4 @@
 #!/bin/sh
-echo "Starting Vault init..."
 export VAULT_ADDR='http://vault:8200'
-
-echo "Waiting for Vault API..."
-until vault status > /dev/null 2>&1; do
-  echo "  Still waiting..."
-  sleep 2
-done
-
-echo "Vault is ready. Logging in..."
-vault login devops-lw3-root-token || exit 1
-
-echo "Writing secrets..."
-vault kv put secret/db \
-  DB_HOST=db \
-  DB_PORT=5432 \
-  DB_USER=postgres \
-  DB_PASS=postgres \
-  DB_NAME=glass_db || exit 1
-
-echo "Vault secrets initialized successfully."
+echo "Waiting for Vault API..." && until vault status > /dev/null 2>&1; do echo "  Still waiting..." && sleep 2; done
+echo "Vault is ready. Logging in..." && vault login devops-lw3-root-token && echo "Writing secrets..." && vault kv put secret/db DB_HOST=db DB_PORT=5432 DB_USER=postgres DB_PASS=postgres DB_NAME=glass_db && echo "Vault secrets initialized successfully."
